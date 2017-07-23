42141ba3000f9969f99616b1307b01f19ed51087
tomiskym <tomiskym@gmail.com>
2017-07-23 23:40:40 +0200

    Change Appearance, Repair rzSlider, Add tableCtrl2 for second demo
    Change Appearance,
    Repair rzSlider,
    Add tableCtrl2 for second demo

--- demo/index.php
+++ demo/index.php
@@ -34,38 +34,50 @@
 
 </head>
 <body>
-<div class="container">
+
 <header>
-  <h1>
-    <a href="">AngularJS Table - Selection of residences / apartments</a>
-  </h1>
-  Using: Bootstrap 3.3.7, Font Awesome 4.7, Jquery 3.2.1, LightGallery.js (Optional - Turned Off), Angular 1.6.4, HTML5 Shiv 3.7.3, Respond 1.4.2, Responsive
+  <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="col-lg-12">
+        <a class="navbar-brand" href="#">AngularJS Table - Selection of residences / apartments</a>
 
-  <div>
 
 
-    <div class="alert alert-info" role="alert">This demo contains:
-      <ul>
-  <li>  Sorting (Click Name of column)</li>
-  <li>  Filtering by checkbox(Click Checkbox)</li>
-  <li>  Filtering by range slider (Select price on slider)</li>
-  </ul>
     </div>
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#demo1" aria-controls="demo1" role="tab" data-toggle="tab">Demo 1 - Made with table</a></li>
-    <li role="presentation"><a href="#demo2" aria-controls="demo2" role="tab" data-toggle="tab">Demo 2 - Made with divs</a></li>
-  </ul>
+    <div class="col-lg-12">
+    <i class="fa fa-github"> </i> <a href="https://github.com/tomiskym/AngularJS-Sortable-Responsive-Table---available-apartments---properties">/tomiskym/AngularJS-Sortable-Responsive-Table---available-apartments---properties</a>
+    </div>
+  </div><!-- /.container-fluid -->
+  </nav>
 
-
-
-
-</div>
 
 </header>
+<div class="container">
 
+
+    <div>
+
+
+      <div class="alert alert-info" role="alert">This demo contains:
+        <ul>
+    <li>  Sorting (Click Name of column)</li>
+    <li>  Filtering by checkbox(Click Checkbox)</li>
+    <li>  Filtering by range slider (Select price on slider)</li>
+    </ul>
+      </div>
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#demo1" aria-controls="demo1" role="tab" data-toggle="tab">Demo 1 - Made with table</a></li>
+      <li role="presentation"><a href="#demo2" aria-controls="demo2" role="tab" data-toggle="tab">Demo 2 - Made with divs</a></li>
+    </ul>
+
+
+
+
+  </div>
 <!-- Tab panes -->
-<div class="tab-content" ng-app="table">
+<div class="tab-content" ng-app="table" >
   <div role="tabpanel" class="tab-pane active fade in" id="demo1">
     <div  ng-controller="tableCtrl" >
 
@@ -73,13 +85,43 @@
 
 
     <div class="filters-section">
-      <input id="checkbox-1" class="checkbox-custom" data-ng-model='filters.available' name="checkbox-1" type="checkbox" >
-      <label for="checkbox-1" class="checkbox-custom-label"> Available Only</label>
-      <input id="checkbox-2" class="checkbox-custom" data-ng-model='filters.terrace' name="checkbox-2" type="checkbox" >
-      <label for="checkbox-2" class="checkbox-custom-label"> With Terrace</label>
-      <input id="checkbox-3" class="checkbox-custom" data-ng-model='filters.balcony' name="checkbox-3" type="checkbox">
-      <label for="checkbox-3" class="checkbox-custom-label"> With Balcony</label>
 
+      <div class="form-group">
+        <input type="checkbox"  data-ng-model='filters.available'  name="fancy-checkbox-default1" id="fancy-checkbox-default1" autocomplete="off" />
+        <div class="btn-group">
+            <label for="fancy-checkbox-default1" class="btn btn-default">
+                <span class="glyphicon glyphicon-ok"></span>
+                <span> </span>
+            </label>
+            <label for="fancy-checkbox-default1" class="btn btn-default active">
+                Available Only
+            </label>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="checkbox"  data-ng-model='filters.terrace'  name="fancy-checkbox-default2" id="fancy-checkbox-default2" autocomplete="off" />
+        <div class="btn-group">
+            <label for="fancy-checkbox-default2" class="btn btn-default">
+                <span class="glyphicon glyphicon-ok"></span>
+                <span> </span>
+            </label>
+            <label for="fancy-checkbox-default2" class="btn btn-default active">
+                With Terrace
+            </label>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="checkbox"  data-ng-model='filters.balcony'  name="fancy-checkbox-default3" id="fancy-checkbox-default3" autocomplete="off" />
+            <div class="btn-group">
+                <label for="fancy-checkbox-default3" class="btn btn-default">
+                    <span class="glyphicon glyphicon-ok"></span>
+                    <span> </span>
+                </label>
+                <label for="fancy-checkbox-default3" class="btn btn-default active">
+                    With Balcony
+                </label>
+            </div>
+      </div>
       <rzslider rz-slider-model="slider.minValue"
               rz-slider-high="slider.maxValue"
               rz-slider-options="slider.options" class="filter-price"></rzslider>
@@ -155,8 +197,8 @@
           <td>{{residence.metres}} m<sup>2</sup></td>
           <td>{{residence.rooms}} Rooms</td>
           <td>{{residence.floor}} Floor</td>
-          <td>Terrace<i class="fa fa-check" ng-show="{{residence.terrace}}"></i></td>
-          <td>Balcony<i class="fa fa-check" ng-show="{{residence.balcony}}"></i></td>
+          <td><i class="fa fa-check" ng-show="{{residence.terrace}}"></i></td>
+          <td><i class="fa fa-check" ng-show="{{residence.balcony}}"></i></td>
           <td>{{residence.price }} zł</td>
           <td>Images:
             <div id="lightgallery">
@@ -173,7 +215,7 @@
   </div>
   <div role="tabpanel" class="tab-pane fade" id="demo2">
 
-    <div ng-controller="tableCtrl" >
+    <div ng-controller="tableCtrl2" >
 
   <!--Filters -->
 
@@ -181,37 +223,37 @@
     <div class="filters-section">
 
       <div class="form-group">
-        <input type="checkbox"  data-ng-model='filters.available'  name="fancy-checkbox-default1" id="fancy-checkbox-default1" autocomplete="off" />
+        <input type="checkbox"  data-ng-model='filters.available'  name="fancy-checkbox-default4" id="fancy-checkbox-default4" autocomplete="off" />
         <div class="btn-group">
-            <label for="fancy-checkbox-default1" class="btn btn-default">
+            <label for="fancy-checkbox-default4" class="btn btn-default">
                 <span class="glyphicon glyphicon-ok"></span>
                 <span> </span>
             </label>
-            <label for="fancy-checkbox-default1" class="btn btn-default active">
+            <label for="fancy-checkbox-default4" class="btn btn-default active">
                 Available Only
             </label>
         </div>
       </div>
       <div class="form-group">
-        <input type="checkbox"  data-ng-model='filters.terrace'  name="fancy-checkbox-default2" id="fancy-checkbox-default2" autocomplete="off" />
+        <input type="checkbox"  data-ng-model='filters.terrace'  name="fancy-checkbox-default5" id="fancy-checkbox-default5" autocomplete="off" />
         <div class="btn-group">
-            <label for="fancy-checkbox-default2" class="btn btn-default">
+            <label for="fancy-checkbox-default5" class="btn btn-default">
                 <span class="glyphicon glyphicon-ok"></span>
                 <span> </span>
             </label>
-            <label for="fancy-checkbox-default2" class="btn btn-default active">
+            <label for="fancy-checkbox-default5" class="btn btn-default active">
                 With Terrace
             </label>
         </div>
       </div>
       <div class="form-group">
-        <input type="checkbox"  data-ng-model='filters.balcony'  name="fancy-checkbox-default3" id="fancy-checkbox-default3" autocomplete="off" />
+        <input type="checkbox"  data-ng-model='filters.balcony'  name="fancy-checkbox-default6" id="fancy-checkbox-default6" autocomplete="off" />
             <div class="btn-group">
-                <label for="fancy-checkbox-default3" class="btn btn-default">
+                <label for="fancy-checkbox-default6" class="btn btn-default">
                     <span class="glyphicon glyphicon-ok"></span>
                     <span> </span>
                 </label>
-                <label for="fancy-checkbox-default3" class="btn btn-default active">
+                <label for="fancy-checkbox-default6" class="btn btn-default active">
                     With Balcony
                 </label>
             </div>
@@ -226,22 +268,24 @@
 
 
         <div class="divtable col-lg-12" ng-repeat="residence in residences | orderBy:sortType:sortReverse | filter: maxFilter | filter: minFilter | customFilter:filters as filteredResidences" >
+          <div class="row">
+          <div class="col-xs-6 col-sm-3  border" ng-class="{'green': residence.status =='Available', 'red': residence.status == 'Sold','grey': residence.status =='Reservation'}">{{residence.status}}</div>
+          <div class="col-xs-12 col-sm-12" ng-class="{'green': residence.status =='Available', 'red': residence.status == 'Sold','grey': residence.status =='Reservation'}">{{residence.price }} zł</div>
+          <div class="col-xs-6 col-sm-3  border">{{residence.number}}</div>
+          <div class="col-xs-6 col-sm-3  border">{{residence.metres}} m<sup>2</sup></div>
+          <div class="col-xs-6 col-sm-3  border">{{residence.rooms}} Rooms</div>
+          <div class="col-xs-6 col-sm-3  border">{{residence.floor}} Floor</div>
+          <div class="col-xs-6 col-sm-3  border"><span ng-show="{{residence.terrace}}">Terrace <i class="fa fa-check"></i></span></div>
+          <div class="col-xs-6 col-sm-3  border"><span ng-show="{{residence.balcony}}">Balcony <i class="fa fa-check"></i></span></div>
 
-          <div class="col-xs-6" ng-class="{'green': residence.status =='Available', 'red': residence.status == 'Sold','grey': residence.status =='Reservation'}">{{residence.status}}</div>
-          <div class="col-xs-6">{{residence.number}}</div>
-          <div class="col-xs-6">{{residence.metres}} m<sup>2</sup></div>
-          <div class="col-xs-6">{{residence.rooms}} Rooms</div>
-          <div class="col-xs-6">{{residence.floor}} Floor</div>
-          <div class="col-xs-6">Terrace<i class="fa fa-check" ng-show="{{residence.terrace}}"></i></div>
-          <div class="col-xs-6">Balcony<i class="fa fa-check" ng-show="{{residence.balcony}}"></i></div>
-          <div class="col-xs-12">{{residence.price }} zł</div>
-          <div class="col-xs-12 col-sm-4">Images:
+          <div class="col-xs-12 col-sm-12 border">Images:
             <div id="lightgallery">
                 <span ng-repeat="image in residence.images">
-                  <a href="{{image}}"><img ng-src="{{image}}"/></a>
+                  <a href="{{image}}"><img ng-src="{{image}}" img-hover/></a>
                 </span>
             </div>
           </div>
+        </div>
         </div>
         <div class="col-lg-12" ng-show="filteredResidences.length === 0">No available residences</div>
     </div>
@@ -254,7 +298,7 @@
 
 
 <footer>
-  &copy; 2017 linkedin.com/in/tomasz-malecki
+  &copy; 2017 <a href="http://linkedin.com/in/tomasz-malecki">linkedin.com/in/tomasz-malecki</a>
 </footer>
 </div>
 <script type="text/javascript">
--- demo/table.js
+++ demo/table.js
@@ -38,6 +38,44 @@ app.controller('tableCtrl', ['$scope', '$http',function($scope,$http) {
 
 }]);
 
+app.controller('tableCtrl2', ['$scope', '$http',function($scope,$http) {
+  $scope.slider = {
+      minValue: 200000,
+      maxValue: 350000,
+      options: {
+          floor: 200000,
+          ceil: 350000,
+          step: 1000,
+          translate: function(value) {
+            return value + ' $';
+          }
+      }
+  };
+  $scope.sortType     = 'number'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  //All filters not used by default
+  $scope.filters = {available: false, terrace: false, balcony: false};
+  $scope.residences = [ ];
+
+  $scope.maxFilter = function (item) {
+      return item.price <= $scope.slider.maxValue;
+  };
+  $scope.minFilter = function (item) {
+      return item.price >= $scope.slider.minValue;
+  };
+
+  $http.get('residences.json')
+    .then(function mySuccess(response) {
+      $scope.residences = response.data;
+      console.log(response, 'yee data.');
+    }, function myError(response) {
+        $scope.residence = response.statusText;
+        console.log('can not get data.');
+    });
+
+
+}]);
+
 //Filter for checkboxes and slider
 app.filter('customFilter', function() {
 
